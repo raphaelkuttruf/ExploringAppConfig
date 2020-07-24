ee4567cf80fbc139897d8e1e9fbc945d5f8108fa
Raphael Kuttruf <Raphael.Kuttruf@vollert.de>
2020-07-24 10:40:41 +0200

    Implemented reading and modifieing appSettings by using System.Configuration

--- ExploringAppConfig/ExploringAppConfig/Program.cs
+++ ExploringAppConfig/ExploringAppConfig/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 
 namespace ExploringAppConfig
 {
@@ -6,8 +7,25 @@ namespace ExploringAppConfig
     {
         static void Main(string[] args)
         {
-            string username = "Raphael Kuttruf";
-            Console.WriteLine($"Hello {username}");
+            string userName = "Raphael Kuttruf";
+            Console.WriteLine($"Hello {userName}");
+
+            // read settings
+            string userNameInAppConfig = ConfigurationManager.AppSettings["UserName"];
+            string userIDInAppConfig = ConfigurationManager.AppSettings["UserID"];
+            Console.WriteLine($"Your configured user name is: {userNameInAppConfig}");
+            Console.WriteLine($"Your configured user ID is: {userIDInAppConfig}");
+
+            // Change setting at runtime and save it
+            Console.WriteLine("I can simplify your settings for you...");
+            int userID = Int32.Parse(userIDInAppConfig);
+            ConfigurationManager.AppSettings["UserID"] = userID.ToString();
+
+            // read again
+             userNameInAppConfig = ConfigurationManager.AppSettings["UserName"];
+             userIDInAppConfig = ConfigurationManager.AppSettings["UserID"];
+            Console.WriteLine($"Your configured user name is: {userNameInAppConfig}");
+            Console.WriteLine($"Your configured user ID is: {userIDInAppConfig}");
 
             Console.ReadLine();
         }
